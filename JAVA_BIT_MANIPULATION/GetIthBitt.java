7196daecbfeb45c3d72a03ffa1facb8dacb19408
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-12-22 22:58:07 +0530

    Get Ith Bit

---
+++ JAVA_BIT_MANIPULATION/GetIthBitt.java
@@ -0,0 +1,21 @@
+package JAVA_BIT_MANIPULATION;
+
+import java.util.Scanner;
+
+public class GetIthBitt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = sc.nextInt();
+        int result = getIthBit(n, i);
+        System.out.println("Ith bit is: " + result);
+        sc.close();
+    }
+
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0)
+            return 0;
+        return 1;
+    }
+}

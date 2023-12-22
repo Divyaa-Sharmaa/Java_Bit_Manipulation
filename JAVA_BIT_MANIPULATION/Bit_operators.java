100fe99904443d4154d7d234e19127ea9e5f0742
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-12-22 22:44:35 +0530

    Bit operators

--- JAVA_BIT_MANIPULATION/Bit_operators.java
+++ JAVA_BIT_MANIPULATION/Bit_operators.java
@@ -6,20 +6,21 @@ class Bit_operators {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        boolean isEvenOrOdd = checkIfEvenOrOdd(x);
-        if (isEvenOrOdd) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
-        }
+        int y = sc.nextInt();
+        // 1. Binary AND (&)
+        System.out.println(x + "&" + y + "= " + (x & y));
+        // Binary Or (|)
+        System.out.println(x + "|" + y + "= " + (x | y));
+        // XOR Operator ^
+        System.out.println(x + "^" + y + "= " + (x ^ y));
+        // Binary 1's compliment
+        System.out.println("~" + x + "= " + ~x);
+        // Right shift >>
+        System.out.println(x + ">>" + y + "= " + (x >> y));
+        // Left Shift <<
+        System.out.println(x + "<<" + y + "= " + (x << y));
+
         sc.close();
     }
 
-    public static boolean checkIfEvenOrOdd(int x) {
-        int bitmask = 1;
-        if ((x & bitmask) == 1) {
-            return false;
-        }
-        return true;
-    }
 }

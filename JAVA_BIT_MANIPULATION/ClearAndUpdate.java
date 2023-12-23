a3b0975c9cf1a70793ba8e5da90e6ad806e0c69e
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-12-23 08:22:25 +0530

    Clear and update Ith Bit

--- JAVA_BIT_MANIPULATION/ClearAndUpdate.java
+++ JAVA_BIT_MANIPULATION/ClearAndUpdate.java
@@ -7,12 +7,25 @@ public class ClearAndUpdate {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int i = sc.nextInt();
-        System.out.println(clearIthBit(x, i)); // 16
+        int newBit = sc.nextInt();
+        // System.out.println(clearIthBit(x, i));
+        System.out.println(updateIthBit(x, i, newBit));
         sc.close();
     }
 
     public static int clearIthBit(int n, int i) {
         int bitMask = ~(1 << i);
         return (n & bitMask);
+    }
+
+    public static int updateIthBit(int n, int i, int newBit) {
+        if (newBit == 0)
+            return clearIthBit(n, i); // return (n & (~(1<<i)));
+        else
+            return (n | (1 << i));
+        // or we can do it by another trick as follow:
+        // n = clearIthBit(n, i);
+        // int bitMask = newBit << i;
+        // return (n | bitMask);
     }
 }
